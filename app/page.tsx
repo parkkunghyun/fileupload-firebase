385f1f01b9952dce01f2700ddc767ef632d23a9d
parkkunghyun <rudgus4620@gmail.com>
2025-01-22 23:47:55 +0900

    main page ui completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,87 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { auth } from "@/firebaseConfig"; // Firebase 인증 설정
+import { onAuthStateChanged } from "firebase/auth";
+import { FaArrowRight } from "react-icons/fa";
+
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const [user, setUser] = useState<any>(null);
+  const router = useRouter();
+
+  // 인증 상태 확인
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+
+  const handleTryItNow = () => {
+    if (!user) {
+      toast.error("로그인 후 사용해 주세요!", {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return;
+    }
+    router.push("/dashboard");
+  };
+
   return (
     <main>
-      hello
+      <div className="flex flex-col mt-4 items-center lg:flex-row bg-[#1E1919] dark:bg-slate-800">
+        <div className="p-10 bg-[#282929] dark:bg-slate-800 flex flex-col text-white space-y-5">
+          <h1 className="text-4xl font-bold">
+            {user ? `${user.email.split("@")[0]}님, Drop In File에 오신 것을 환영합니다!`
+              : "Drop In File에 오신 것을 환영합니다!"}
+            <br />
+          </h1>
+          <p className="pb-10">
+          편하게 드래그로 파일을 업로드하고 저장한 파일을 원하실 때 어디서든지 다운받으세요. <br /><br/>
+          지금 바로 시작해서 더 간편한 파일 관리의 세계를 경험해보세요!
+          </p>
+
+          <button
+            onClick={handleTryItNow}
+            className="bg-blue-400 w-[150px] justify-center py-2 rounded-md px-4 hover:scale-105 flex items-center gap-1 text-white"
+          >
+            Try It Now!
+            <FaArrowRight className="text-xl" />
+          </button>
+        </div>
+
+        <div className="bg-[#1#1919] dark:bg-slate-800 h-full p-10">
+          <video autoPlay loop muted className="rounded-lg w-full h-auto">
+            <source
+              src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+
+      <p className="text-center font-bold text-xl pt-5">Developer: Park Kyung Hyun</p>
+      <p className="text-center font-light p-2">
+        구글 드라이브와 같은 직관적인 파일 저장 시스템에서 영감을 받아, 개인화된 파이어베이스 기반의 드라이브를 개발하였습니다. <br />
+        이 시스템은 사용자가 언제 어디서나 편리하게 파일을 저장하고 관리할 수 있도록 도와줍니다.
+      </p>
+      <ToastContainer />
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,47 +1,99 @@
-// components/Header.tsx
 "use client";
 
 import Image from "next/image";
 import React, { useState, useEffect } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
+import { signInWithPopup, onAuthStateChanged, signOut } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { auth, googleProvider } from "@/firebaseConfig";
+import Link from "next/link";
 
 const Header = () => {
-  const [darkMode, setDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
+    const [user, setUser] = useState<any>(null);
+    const router = useRouter();
 
-  // 다크 모드 상태에 따라 <html> 태그에 클래스 추가/제거
-  useEffect(() => {
-    const html = document.documentElement;
-    if (darkMode) {
-      html.classList.add("dark");
-    } else {
-      html.classList.remove("dark");
+    // 다크 모드 상태 관리
+    useEffect(() => {
+        const html = document.documentElement;
+        if (darkMode) {
+            html.classList.add("dark");
+        } else {
+            html.classList.remove("dark");
+        }
+    }, [darkMode]);
+
+    // 인증상태 확인 - 현재 로그인 중인지 확인합니다. 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                setUser(currentUser);
+            } else {
+                setUser(null);
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+
+    // google OAuth로 로그인
+    const handleGoogleLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+            setUser(result.user);
+            router.push("/dashboard"); // 로그인 후 대시보드로 이동합니다.
+        } catch (error) {
+            console.error("Google 로그인에 실패했습니다", error);
+        }
     }
-  }, [darkMode]);
 
-  const toggleDarkMode = () => {
-    setDarkMode((prev) => !prev);
-  };
+    // 로그아웃 핸들러
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+            setUser(null);
+        } catch (error) {
+            console.error("Logout error", error);
+        }
+    };
 
     return (
         <header className="flex items-center justify-between px-6 py-2 bg-gray-100 dark:bg-gray-800 shadow-md">
-          <div className="flex items-center gap-2">
+          <Link href="/" className="flex items-center gap-2">
             <Image src="/dropbox-logo.png" className="dark:invert" alt="logo image" width={100} height={100} />
-              <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-100">Drop In File</h1>
-          </div>
-            <div className="flex items-center gap-4">
-                <button className="flex items-center bg-white p-1 shadow-lg rounded-lg hover:scale-105 duration-300" >
-                    <Image src="/google-image.png" alt="google logo" width={30} height={30} />
-                    <span className="font-bold dark:text-black">로그인</span>
-                </button>
+            <h1 className="text-2xl font-bold text-gray-800 dark:text-gray-100">Drop In File</h1>
+          </Link>
+          <div className="flex items-center gap-4">
+            {user ? (
+              <div className="flex items-center gap-4">
+                <span className="text-gray-800 dark:text-gray-100 font-medium">
+                  {user.email.split("@")[0]}님 반갑습니다.
+                </span>
                 <button
-                    onClick={toggleDarkMode}
-                    className="p-2 rounded-full bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-gray-100 shadow-md"
-                    aria-label="Toggle Dark Mode">
-                    {darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
+                  onClick={handleLogout}
+                  className="flex items-center bg-red-400 text-white p-2 rounded-lg shadow-lg hover:scale-105 duration-300"
+                >
+                  로그아웃
                 </button>
-            </div>
-    </header>
-  );
-};
-
+              </div>
+            ) : (
+              <button
+                onClick={handleGoogleLogin}
+                className="flex items-center bg-white p-1 shadow-lg rounded-lg hover:scale-105 duration-300"
+              >
+                <Image src="/google-image.png" alt="google logo" width={30} height={30} />
+                <span className="font-bold dark:text-black">로그인</span>
+              </button>
+            )}
+            <button
+              onClick={() => setDarkMode((prev) => !prev)}
+              className="p-2 rounded-full bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-gray-100 shadow-md"
+              aria-label="Toggle Dark Mode"
+            >
+              {darkMode ? <FaSun size={20} /> : <FaMoon size={20} />}
+            </button>
+          </div>
+        </header>
+      );
+    };
+    
 export default Header;
